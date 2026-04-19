58e381caa4469ae8104bdacca8c1c25079f9cb33
MarcosEscandon <marcos.escandon@gmail.com>
2026-04-19 08:51:29 -0300

    Make orderNumber optional since it's auto-generated by middleware

--- server/models/Order.js
+++ server/models/Order.js
@@ -25,7 +25,6 @@ const orderItemSchema = new mongoose.Schema({
 const orderSchema = new mongoose.Schema({
   orderNumber: {
     type: String,
-    required: true,
     unique: true
   },
   customerName: {
