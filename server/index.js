8b8d20554d6e6daf74f545eb76de90e5fff151ab
MarcosEscandon <marcos.escandon@gmail.com>
2026-04-19 08:06:01 -0300

    Fix deprecated MongoDB options (charset, useFindAndModify)

--- server/index.js
+++ server/index.js
@@ -36,12 +36,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cafe-menu', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  charset: 'utf8',
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cafe-menu');
 
 // Socket.IO connection handling
 io.on('connection', (socket) => {
