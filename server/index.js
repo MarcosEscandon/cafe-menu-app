89a06efaa0d29e6c5752923291adde1ffdce66f5
MarcosEscandon <marcos.escandon@gmail.com>
2026-04-19 08:41:31 -0300

    Remove deprecated MongoDB connection options (bufferMaxEntries)

--- server/index.js
+++ server/index.js
@@ -42,10 +42,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cafe-menu', {
-  bufferCommands: false,
-  bufferMaxEntries: 0
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cafe-menu');
 
 // Socket.IO connection handling
 io.on('connection', (socket) => {
